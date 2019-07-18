2838611c02fd1d5b66a66eb622d6c4396ffcc4c4
garyzouchg <522702305@qq.com>
2019-07-18 18:13:07 +0800

    confulence deploy

---
+++ confulence_deploy.bash
@@ -0,0 +1,8 @@
+
+useradd -r -m -s /bin/bash me
+usermod -aG sudo me
+
+https://download.oracle.com/otn/java/jdk/8u221-b11/230deb18db3e4014bb8e3e8324f81b43/jdk-8u221-linux-x64.tar.gz
+
+
+https://www.atlassian.com/software/confluence/download
--- eth/eth_usage.bash
+++ eth/eth_usage.bash
@@ -1,5 +1,17 @@
 aleth --ropsten
 
-JSONRPC Admin Session Key: 2iumXJQRO54=
+
+brew install nodejs
+npm install web3
+npm install solc -g
+solcjs simpleStorage.sol --abi
+
+
+curl -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://127.0.0.1:8545
+
+
+solidity
+
+
 
 
