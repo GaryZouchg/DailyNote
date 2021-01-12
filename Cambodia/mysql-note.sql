56bd055172b71d2cccdf696f8dbb49e8a7912647
garyzouchg <522702305@qq.com>
2021-01-12 23:48:32 +0800

    add db and user

--- Cambodia/mysql-note.sql
+++ Cambodia/mysql-note.sql
@@ -1,38 +1,42 @@
+--ubuntu18   mysql5.7
 sudo apt mysql-server
 sudo apt-get upgrade mysql-server
-
-mysqld --console
-
+sudo apt-get install nmap
 nmap -Pn -p3306 140.143.138.127
- 
-mysqladmin -uroot shutdown
 
+--启动mysql
+service mysql status/start/restart/stop 
+mysqladmin -uroot shutdown
+mysqladmin -u root -p create TESTDB
+
+--配置mysql
 mysql -u root -p
 show variables like 'secu%';
 show variables like 'char%';
-
-mysqladmin -u root -p create TESTDB
-
 sudo vim /etc/mysql/mysql.conf.d/mysqld.cnf
 secure_file_priv ="/"
 bind-address            = 0.0.0.0
+character_set_database=utf8
+character_set_server=utf8
 
-character-set-database=utf8
-character-set-server=utf8
-
-show variables like 'char%';
-
-
-
-
-
- grant all privileges on *.* to 'root'@'%' identified by 'Ouae0902';
+mysql -u root -p
+grant all privileges on *.* to 'root'@'%' identified by '********';
 flush privileges;​
 
+--创建数据库表
+CREATE DATABASE IF NOT EXISTS DRMNY;
+TRADE_00  TRADE_01  TRADE_02
+--创建用户
+CREATE USER dbadmin@'%' IDENTIFIED BY 'Cambodia0';
+grant all privileges on *.* to 'dbadmin'@'%' identified by 'Cambodia0';
+SHOW GRANTS FOR dbadmin;
 
 
-
-
+CREATE USER user1@'%' IDENTIFIED BY 'Cambodia1';
+grant all privileges on DRMNY.TRADE_01 to 'user1'@'%' identified by 'Cambodia1';
+CREATE USER user2@'%' IDENTIFIED BY 'Cambodia2';
+grant all privileges on DRMNY.TRADE_02 to 'user2'@'%' identified by 'Cambodia2';
+commit;
 
 
 
--- Cambodia/trade_00.sql
+++ Cambodia/trade_00.sql
@@ -1,20 +1,34 @@
-  drop table trade_00;
-  CREATE TABLE IF NOT EXISTS trade_00(
-   action                VARCHAR(20) NOT NULL,
-   cust_name             VARCHAR(64) NOT NULL,
-   contact               VARCHAR(64) NOT NULL,
-   facebook              VARCHAR(64) ,
-   passport              VARCHAR(20) NOT NULL,
-   cust_bank_account     VARCHAR(20) NOT NULL,
-   lending_amount        int NOT NULL,
-   interest              int NOT NULL,
-   repayment_amount      int,
-   recieve_bank_account  VARCHAR(20) ,
-   lending_date          date,
-   payment_due_date      date,
-   actual_payment_date   date,
-   trade_state           VARCHAR(20) NOT NULL,
-   comment1              VARCHAR(1024),
-   comment2              VARCHAR(1024),
-   PRIMARY KEY ( passport,lending_date )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table TRADE_00;
+CREATE TABLE IF NOT EXISTS TRADE_00(
+	action             VARCHAR(10)  NOT NULL  COMMENT '交易操作'    ,
+	trade_state        VARCHAR(10)  NOT NULL  COMMENT '交易状态'    ,
+	cust_name          VARCHAR(64)  NOT NULL  COMMENT '客户姓名'    ,
+	passport           VARCHAR(20)  NOT NULL  COMMENT '身份证/护照' ,
+	out_bank_acct      VARCHAR(20)  NOT NULL  COMMENT '放款账号'    ,
+	cust_bank_acct     VARCHAR(20)  NOT NULL  COMMENT '客户收款账户',
+	lending_date       date         NOT NULL  COMMENT '借款日期',
+	lending_amount     int          NOT NULL  COMMENT '借款金额',
+	due_date           date         NOT NULL  COMMENT '到期日',
+	due_interest       int          NOT NULL  COMMENT '应还利息', 
+	repayment_date     date                   COMMENT '实际还款日期',
+	repayment_amount   int                    COMMENT '借款金额',
+	repayment_total    int                    COMMENT '借款金额',
+	repayment_acct     VARCHAR(20)            COMMENT '还入账户',
+	contact            VARCHAR(20)            COMMENT '联系方式',
+	facebook_acct      VARCHAR(20)            COMMENT 'FB账号',
+	comment1           VARCHAR(1024)          COMMENT '备注1',
+	comment2           VARCHAR(1024)          COMMENT '备注2',
+	PRIMARY KEY ( passport,lending_date )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT '交易表00';
+
+CREATE INDEX TRADE_00_idx ON TRADE_00 (repayment_date,cust_name);
+
+commit;
+	
+	
+	
+	
+	
+	
+	
+ 	
